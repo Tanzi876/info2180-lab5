fbafa2201242b3740b26e6438130790b4b85381b
Tanzi876 <tanyaleelawrence@gmail.com>
2020-11-13 16:20:58 -0500

    Final edit

---
+++ world.js
@@ -0,0 +1,34 @@
+/*window.onload=*/function(){
+    let country_search=document.getElementById('lookup_countries')
+    let city_search=document.getElementById('lookup_cities')
+    let request=new XMLHttpRequest()
+    let result=document.getElementById('result')
+    
+    country_search.onclick=function(){
+        let search=document.getElementById("country").value
+        let url= 'world.php?country='+search+"&context=country"
+        request.open('GET',url)
+        request.send()
+        request.onload=function(){
+            if(this.readyState==4 && this.status==200){
+                result.innerHTML=this.responseText
+            }
+
+        }
+
+
+    }
+    city_search.onclick=function(){
+        let search=document.getElementById("country").value
+        let url= 'world.php?country='+search+"&context=cities"
+        request.open('GET',url)
+        request.send()
+        request.onload=function(){
+            if(this.readyState==4 && this.status==200){
+                result.innerHTML=this.responseText
+            }
+
+        }
+
+    }
+}
